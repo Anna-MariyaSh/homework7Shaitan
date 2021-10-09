55cab4beed27c36a4b015962f1d51ae84e5dbf87
Anna-MariyaSh <88891571+Anna-MariyaSh@users.noreply.github.com>
2021-10-09 21:48:01 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,36 @@
+let a = 10;
+// a = 0 ? (alert("Верно")):(alert("Неверно"));
+// a > 0 ? (alert("Верно")):(alert("Неверно"));
+// a < 0 ? (alert("Верно")):(alert("Неверно"));
+// a >= 0 ? (alert("Верно")):(alert("Неверно"));
+// a <= 0 ? (alert("Верно")):(alert("Неверно"));
+// a != 0 ? (alert("Верно")):(alert("Неверно"));
+// a === "test" ? (alert("Верно")):(alert("Неверно"));
+// a === "1" ? (alert("Верно")):(alert("Неверно"));
+//a = 0 && a = 2? (alert("Верно")):(alert("Неверно"));
+//if (a === 0 || a === 2) {
+//   a = a + 7;
+//} else {
+//  a = a / 10;
+//}
+//alert(a);
+let b = 3;
+// if (a<=1 && b>=3 ) {
+  // alert(a+b)
+// } else {
+  // alert(a-b)
+// }
+// if( (a>2 && a<11)  || (b>=6 && b<14)) {
+  // alert("Верно")
+// } else {
+  // alert("Неверно")
+// }
+let num = 1;
+switch (num) {
+  case 1: alert("Зима");break;
+  case 2: alert("Весна");break;
+  case 4: alert("Лето");break;
+  case 3: alert("Осень");break;
+  default: alert("Error")
+
+}
